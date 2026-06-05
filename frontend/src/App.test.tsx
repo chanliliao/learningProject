7dd0b3fb059f22a566380f99c4bd4bca0453f14e
Chan Li Liao <cliao119@gmail.com>
2026-06-04 22:21:48 -0400

    test: strengthen App render test assertion

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,5 +3,5 @@ import App from './App'
 
 test('renders app without crashing', () => {
   render(<App />)
-  expect(document.body).toBeTruthy()
+  expect(screen.getByRole('heading', { name: /get started/i })).toBeInTheDocument()
 })
